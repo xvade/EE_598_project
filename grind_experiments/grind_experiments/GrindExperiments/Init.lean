da1c81883450b81604d182b02550981c849cef83
xvade <simon@vantuyl.com>
2026-03-11 10:22:35 -0700

    starting to work with grind tactic internals

---
+++ grind_experiments/grind_experiments/GrindExperiments/Init.lean
@@ -0,0 +1,5 @@
+import Mathlib.Init
+import Mathlib.Tactic.Common
+import GrindExperiments.MyLint
+
+register_grind_attr my_grind_attr
---
+++ grind_experiments/grind_experiments/GrindExperiments/People.lean
@@ -0,0 +1,23 @@
+import GrindExperiments.Init
+
+inductive Person where | mary | steve | ed | jolin
+
+open Person
+
+namespace mod
+def on_right (p q : Person) : Prop := match p with
+  | mary => q = steve
+  | steve => q = ed
+  | ed => q = jolin
+  | jolin => q = mary
+
+@[grind .]
+theorem steve_is_the_one (x : Person) (hx : on_right mary x) : x = steve := by sorry
+
+
+@[my_grind_attr .]
+theorem steve_is_the_one' (x : Person) (hx : on_right mary x) : x = steve := by sorry
+
+@[my_grind_attr .]
+theorem the_one_is_steve (x : Person) (hx : on_right mary x) : steve = x := by sorry
+end mod
